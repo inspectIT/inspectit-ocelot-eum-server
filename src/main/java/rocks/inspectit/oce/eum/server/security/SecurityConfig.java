16f4edb13ada58dcc06fd8680315eb9b1f50a03f
EddeCCC <eduard.schander@gmx.de>
2023-06-14 08:43:20 +0200

    refactor SecurityConfig without WebSecurityConfigurerAdapter

--- src/main/java/rocks/inspectit/oce/eum/server/security/SecurityConfig.java
+++ src/main/java/rocks/inspectit/oce/eum/server/security/SecurityConfig.java
@@ -2,12 +2,15 @@ package rocks.inspectit.oce.eum.server.security;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import rocks.inspectit.oce.eum.server.configuration.model.EumServerConfiguration;
 import rocks.inspectit.oce.eum.server.configuration.model.security.SecuritySettings;
@@ -17,11 +20,12 @@ import java.util.List;
 
 @Slf4j
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
     private EumServerConfiguration configuration;
-
     @Autowired(required = false)
     private List<AuthenticationProvider> apiTokenAuthProviders = Collections.emptyList();
 
@@ -30,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      *
      * @param auth the {@link AuthenticationManagerBuilder} to use
      */
-    @Override
+    @Autowired
     protected void configure(AuthenticationManagerBuilder auth) {
         if (configuration.getSecurity().isEnabled()) {
             apiTokenAuthProviders.forEach(auth::authenticationProvider);
@@ -38,38 +42,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     /**
-     * Configures {@link HttpSecurity} if security is enabeld by
+     * Configures {@link HttpSecurity} if security is enabled by
      * <ul>
      *  <li>Permitting all white listed urls in {@link SecuritySettings#getPermittedUrls()}</li>
-     *  <li>Ensure all other request require authentication</li>
+     *  <li>Ensure all other requests require authentication</li>
      *  <li>add {@link ApiTokenFilter} to filter chain</li>
      * </ul>
      *
      * @param http the {@link HttpSecurity} to modify
-     *
      * @throws Exception In case of any error
      */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(AbstractHttpConfigurer::disable).csrf(AbstractHttpConfigurer::disable);
         if (configuration.getSecurity().isEnabled()) {
-            http.authorizeRequests()
-                    .antMatchers(configuration.getSecurity().getPermittedUrls().toArray(new String[]{}))
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated()
-                    .and()
-                    .addFilterAt(new ApiTokenFilter(authenticationManagerBean(), configuration.getSecurity()
-                            .getAuthorizationHeader()), BasicAuthenticationFilter.class);
-
-        } else {
-            http.authorizeRequests().anyRequest().permitAll();
+            http.authorizeHttpRequests(
+                    authz -> authz
+                            .requestMatchers(configuration.getSecurity().getPermittedUrls().toArray(new String[]{}))
+                            .permitAll()
+                            .anyRequest()
+                            .authenticated()
+            ).addFilterAt(
+                    new ApiTokenFilter(authenticationManager, configuration.getSecurity().getAuthorizationHeader()),
+                    BasicAuthenticationFilter.class
+            );
         }
+        else {
+            http.authorizeHttpRequests(
+                    authz -> authz.anyRequest().permitAll()
+            ).httpBasic(Customizer.withDefaults());
+        }
+        return http.build();
     }
-
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
 }
